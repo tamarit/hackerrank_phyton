e008e11412183416be7c5f3ebfc9470cf4b8ff95
Salvador Tamarit <stamarit@software.imdea.org>
2017-07-25 20:03:21 +0200

    py-introduction-to-sets
    https://www.hackerrank.com/challenges/py-introduction-to-sets

---
+++ py-introduction-to-sets/solution.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/py-introduction-to-sets
+
+import statistics
+
+def average(array):
+    # your code goes here
+    return statistics.mean(set(array))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
