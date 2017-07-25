428d3b9c5c411586bcc964c0c6a5ead34ad661da
Salvador Tamarit <stamarit@software.imdea.org>
2017-07-25 15:06:35 +0200

    python-tuples
    https://www.hackerrank.com/challenges/python-tuples

---
+++ python-tuples/solution.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-tuples
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
