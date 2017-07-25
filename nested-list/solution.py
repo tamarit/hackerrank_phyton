a857147cf1896a1182d78f77a78cc899a65caae4
Salvador Tamarit <stamarit@software.imdea.org>
2017-07-25 15:16:37 +0200

    nested-list
    https://www.hackerrank.com/challenges/nested-list

---
+++ nested-list/solution.py
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/nested-list
+
+import sys 
+from operator import itemgetter
+if __name__ == '__main__':
+    lowest_first = sys.float_info.max
+    lowest_second = sys.float_info.max
+    nss = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        nss.append((name, score))
+    lowest_first = min(map(itemgetter(1), nss))
+    lowest_second = sys.float_info.max
+    for (_,s) in nss:
+        if (s < lowest_second) and (s != lowest_first):
+            lowest_second = s
+    same = []
+    for (n,s) in nss:
+        if (s == lowest_second):
+            same.append(n)
+    same.sort()
+    for n in same:
+        print(n)
