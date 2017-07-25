af1d80c493ee0e611b7be3c1a97418d49f05fa98
Salvador Tamarit <stamarit@software.imdea.org>
2017-07-25 15:03:57 +0200

    python-lists
    https://www.hackerrank.com/challenges/python-lists

---
+++ python-lists/solution.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/python-lists
+
+if __name__ == '__main__':
+    N = int(input())
+    cmd = []
+    list = []
+    for i in range(0,N):
+        cmd = input().split()
+        {
+              'insert': 
+                lambda: 
+                    list.insert(int(cmd[1]), int(cmd[2])),
+              'print': 
+                lambda:
+                    print(list),
+              'remove': 
+                lambda:
+                    list.remove(int(cmd[1])),
+              'append': 
+                lambda:
+                    list.append(int(cmd[1])),
+              'sort': 
+                lambda:
+                    list.sort(),
+              'pop': 
+                lambda:
+                    list.pop(),
+              'reverse': 
+                lambda:
+                    list.reverse()
+        }[cmd[0]]()
