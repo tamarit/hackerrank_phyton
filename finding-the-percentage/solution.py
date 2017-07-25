5ba5c9efafc3c3f92d476f7aaebd18a84165ef2b
Salvador Tamarit <stamarit@software.imdea.org>
2017-07-25 15:17:59 +0200

    finding-the-percentage
    https://www.hackerrank.com/challenges/finding-the-percentage

---
+++ finding-the-percentage/solution.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage
+
+import statistics
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print("{0:0.2f}".format(statistics.mean(student_marks[query_name])))
