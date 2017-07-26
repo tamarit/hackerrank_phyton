caa11e3bd1f37aae5b1d1b7edefa83ed84edb28b
Salvador Tamarit <stamarit@software.imdea.org>
2017-07-26 13:17:03 +0200

    py-set-add
    https://www.hackerrank.com/challenges/py-set-add

---
+++ py-set-add/solution.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/py-set-add
+
+if __name__ == '__main__':
+    n = int(input())
+    countries = set()
+    for _ in range(0,n):
+    	countries.add(input().strip())
+    print(list(countries))
+    print(len(countries))
