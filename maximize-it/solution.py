0f9dc85fee5be8c8e77257d8c58bc378e6b2335a
Salvador Tamarit <stamarit@software.imdea.org>
2017-08-12 17:14:29 +0200

    maximize-it
    https://www.hackerrank.com/challenges/maximize-it

---
+++ maximize-it/solution.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/maximize-it
+
+from itertools import combinations
+
+
+_, K = list(input().split(' '))
+L = []
+for i in range(0, K):
+	L.append(input().split(' '))
+print(L)
+
+
+
+
+
+
