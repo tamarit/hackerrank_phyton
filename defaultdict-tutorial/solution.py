85e20e070f7a5ea7d2806f439b03cc6f57a1ac22
Salvador Tamarit <stamarit@software.imdea.org>
2017-08-17 11:37:08 +0200

    defaultdict-tutorial
    https://www.hackerrank.com/challenges/defaultdict-tutorial

---
+++ defaultdict-tutorial/solution.py
@@ -0,0 +1,26 @@
+# https://www.hackerrank.com/challenges/defaultdict-tutorial
+
+from collections import defaultdict
+
+def readn(n):
+	l = []
+	for i in range(0, n):
+		l.append(input().strip())
+	return l
+
+n, m = map(int, list(input().split(' ')))
+A = readn(n)
+B = readn(m)
+d = defaultdict(list)
+for i in B:
+	for j in range(0,len(A)):
+		if i == A[j]:
+			d[i].append(j + 1)
+	if d[i] == []:
+		d[i].append(-1)
+	print(' '.join(map(str, d[i])))
+
+
+
+
+
